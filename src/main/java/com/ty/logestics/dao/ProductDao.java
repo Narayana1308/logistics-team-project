44143ab66a31cf4dcff7a486c16296a920a74368
Tejaprathap18 <thetejaprathap523@gmail.com>
2023-02-24 12:47:32 +0530

    created ProductDao

---
+++ src/main/java/com/ty/logestics/dao/ProductDao.java
@@ -0,0 +1,58 @@
+package com.ty.logestics.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.logestics.dto.Product;
+import com.ty.logestics.repo.ProductRepo;
+
+@Repository
+public class ProductDao {
+
+	@Autowired
+	private ProductRepo repo;
+
+	public Product saveProduct(Product product) {
+
+		return repo.save(product);
+
+	}
+
+	public Product updateProduct(int id, Product product) {
+
+		if (repo.findById(id).isPresent()) {
+			product.setId(id);
+			return repo.save(product);
+		}
+
+		else
+			return null;
+
+	}
+
+	public Product deleteProduct(int id) {
+		if (repo.findById(id).isPresent()) {
+			Product product = repo.findById(id).get();
+			repo.delete(product);
+			return product;
+		} else
+			return null;
+	}
+
+	public Product findById(int id) {
+		if (repo.findById(id).isPresent()) {
+			Product product = repo.findById(id).get();
+			return product;
+
+		} else
+			return null;
+	}
+
+	public List<Product> findAll() {
+		return repo.findAll();
+
+	}
+
+}
